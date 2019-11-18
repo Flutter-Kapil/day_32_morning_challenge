f3329d91313ad5f65f6f873e86e8ee7e612f16a2
Sahil Singh <sahil.dogstar@gmail.com>
2019-11-18 12:17:44 +0530

    wrote test cases for restart method.

--- bin/test.dart
+++ bin/test.dart
@@ -1,4 +1,6 @@
 import 'package:test/test.dart';
+import 'package:test/test.dart';
+import 'package:test/test.dart' as prefix0;
 import 'main.dart';
 void main() {
   Shiritori sOne = Shiritori();
@@ -7,12 +9,17 @@ void main() {
     expect(sOne.play("c"),true);
     expect(sOne.play("cpp"),true);
     expect(sOne.play("python"),true);
-    expect(sOne.play("madesico"),false);
-    expect(sOne.play("lua"),false);
-    expect(sOne.play("assembly"),true);
+    expect(sOne.play("cpp"),false); //duplicate entry
     sOne.restart();
+    expect(sOne.game_over, false);
+    prefix0.expect(sOne.words, []);
+    expect(sOne.play('hal'), true);
+    expect(sOne.play("lua"),true);
+    expect(sOne.play("assembly"),true);
+    expect(sOne.play("java"),false); //last character of assembly and first character of java don't match.
+
+    expect(sOne.restart(), "game restarted");
   });
 }
-
 
 
